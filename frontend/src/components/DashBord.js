c1a177da750a0a3e9d2e5d51757a18b4105d7ae8
A.Divya <108621599+Akkimdivya@users.noreply.github.com>
2024-06-21 12:36:25 +0530

    Update DashBord.js

--- frontend/src/components/DashBord.js
+++ frontend/src/components/DashBord.js
@@ -1,36 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import { Link, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { Link, useParams } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
 const DashBord = () => {
-  let [name, setname] = useState("")
-  let ID = useParams()
+  const [name, setName] = useState("");
+  const { ID } = useParams();
 
-  useEffect(()=>{
-    axios.get(`https://dealday.vercel.app/user/${ID.ID}`)
-    .then((e)=>{
-      setname(e.data)
-    
-    })
-    .catch(()=>{console.log("unable to fetch data in Edit comp");})
-},[])
+  useEffect(() => {
+    axios.get(`https://dealday.onrender.com/user/${ID.ID}`)
+      .then((response) => {
+        setName(response.data.name);
+      })
+      .catch(() => {
+        console.log("Unable to fetch data");
+      });
+  }, [ID]);
 
   return (
     <div>
-      <div id='navbar' className='bg-gray-300  '>
-        <ul className='flex gap-24 px-10'>
-          <li>Home</li>
-          <li><Button variant="text"><Link to='/create-employee'> Create Employee</Link></Button> </li>
-          <li><Button variant="text"><Link to="/employee-list">  Employee list </Link></Button> </li>
-          <li className='p-2 text-red-500 border border-dashed border-red-400 '>{name}</li>
-          <li>Logout</li>
+      <nav className='bg-gray-300 p-4'>
+        <ul className='flex gap-4 justify-center'>
+          <li><Link to='/'>Home</Link></li>
+          <li><Button variant="text"><Link to='/create-employee'>Create Employee</Link></Button></li>
+          <li><Button variant="text"><Link to="/employee-list">Employee List</Link></Button></li>
+          <li className='text-red-500'>{name}</li>
+          <li><Button variant="text"><Link to='/logout'>Logout</Link></Button></li>
         </ul>
-      </div>
-      <h1 className='bg-yellow-200'>DashBord</h1>
-      <p>Wellcome to admin panel</p>
+      </nav>
+      <h1 className='text-center text-2xl mt-6'>Dashboard</h1>
+      <p className='text-center mt-2'>Welcome to the admin panel</p>
     </div>
-  )
-}
+  );
+};
 
 export default DashBord;
